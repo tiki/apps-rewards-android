e10c9eb5d71734406cec363ab8825a70b680c3ad
Ricardo Gonçalves <ricardobrg@users.noreply.github.com>
2023-10-30 09:32:58 -0300

    fix: initial project setup

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Rewards"
+include(":app")
+include(":apps-receipt-rewards")
