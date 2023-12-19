af24b0df1f12f73dc437a2d89eb440084c99d757
Gabriel Schuler Barros <gabrielschuler3@gmail.com>
2023-12-19 12:39:48 -0300

    tests

--- apps-receipt-rewards/src/androidTest/java/com/mytiki/apps_receipt_rewards/RewardsTest.kt
+++ apps-receipt-rewards/src/androidTest/java/com/mytiki/apps_receipt_rewards/RewardsTest.kt
@@ -1,5 +1,9 @@
 package com.mytiki.apps_receipt_rewards
 
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import org.junit.Test
 
 class RewardsTest {
@@ -22,4 +26,23 @@ class RewardsTest {
         assert(Rewards.oauth.outlookAPIKey == "test")
     }
 
+    @Test
+    fun themeTest(){
+        Rewards.theme(
+            Color(0xFF32484E),
+            Color(0x99931A97),
+            Color(0xFF399E22),
+            Color(0xFF0B8069),
+            Color(0xFF5E00F5),
+            FontFamily(
+                Font(R.font.space_grotesk_light, FontWeight.Light), //300
+                Font(R.font.space_grotesk_regular, FontWeight.Normal), //400
+                Font(R.font.space_grotesk_medium, FontWeight.Medium), //500
+                Font(R.font.space_grotesk_semi_bold, FontWeight.SemiBold), //600
+                Font(R.font.space_grotesk_bold, FontWeight.Bold), //700
+            )
+        )
+        assert(Rewards.colorScheme.outline == Color(0xFF32484E))
+        assert(Rewards.colorScheme.primary == Color(0xFF5E00F5))
+    }
 }
